09b45435f3874fe92cba0cada461c91411e021c4
meriem <meriem@meriem.fr>
2016-01-27 17:46:42 +0100

    nom des pages

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #genère le fichier index.html
-rst2html --source-url=https://github.com/lpd-fr/lpd-fr-site -l fr --link-stylesheet --stylesheet=main.css --title="[DRAFT] − Linux Presentation Day France" index.rst  > index.html
-rst2html --source-url=https://github.com/lpd-fr/lpd-fr-site -l fr --link-stylesheet --stylesheet=main.css --title="[DRAFT] − Linux Presentation Day France − Participer" participer.rst > participer.html
-rst2html --source-url=https://github.com/lpd-fr/lpd-fr-site -l fr --link-stylesheet --stylesheet=main.css --title="[DRAFT] − Linux Presentation Day France − Participer" legal.rst > legal.html
+rst2html --source-url=https://github.com/lpd-fr/lpd-fr-site -l fr --link-stylesheet --stylesheet=main.css --title="Linux Presentation Day France" index.rst  > index.html
+rst2html --source-url=https://github.com/lpd-fr/lpd-fr-site -l fr --link-stylesheet --stylesheet=main.css --title="Linux Presentation Day France − Participer" participer.rst > participer.html
+rst2html --source-url=https://github.com/lpd-fr/lpd-fr-site -l fr --link-stylesheet --stylesheet=main.css --title="Linux Presentation Day France − Mentions légales" legal.rst > legal.html
